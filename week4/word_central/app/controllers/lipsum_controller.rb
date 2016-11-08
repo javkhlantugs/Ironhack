ce988e9492a3330c3d8a98e0a3dbd38d56a7f358
javkhlantugs <jay.mudgil@gmail.com>
2016-11-08 10:20:23 -0500

    completed week4 day1 word_central fully

--- week4/word_central/app/controllers/lipsum_controller.rb
+++ week4/word_central/app/controllers/lipsum_controller.rb
@@ -4,6 +4,15 @@ class LipsumController < ApplicationController
 	end
 
 	def create
+		@final = String.new
+		@times = params[:user_input].to_i
+		if params[:choice] == 'paragraph'
+				 @final = "Lorizzle ipsizzle dolizzle sit amet, fo shizzle my nizzle adipiscing boofron. That's the shizzle sapien , that's the shizzle volutpizzle, suscipizzle for sure, dang vizzle, ghetto. Pellentesque shiznit tortor. Sed erizzle. Bow wow wow black dolizzle dapibizzle turpis tempizzle for sure. Maurizzle pellentesque nibh et turpis. Sheezy izzle tortizzle. Nizzle eleifend rhoncizzle . In hac habitasse dang dictumst. Donec dapibizzle. Fo shizzle my nizzle tellus urna, pretizzle , gizzle ac, break it down vitae, fo shizzle. Fizzle suscipizzle. Own yo' sempizzle shit bow wow wow dizzle. "
+		else
+			# render plain: params[:user_input].class
+				@final = "Lorizzle ipsizzle dolizzle sit amet, fo shizzle my nizzle adipiscing boofron. "
+		end
+		@final
 	end
 	
 end
